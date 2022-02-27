7692d07d89ad671bc4dc81fc4ad37747e0a2a7de
Peter <peter.braendgaard@gmail.com>
2022-02-27 11:06:12 +0100

    Static ips

---
+++ SemesterProject/ArduinoCode/Shared/wifi.h
@@ -0,0 +1,8 @@
+#ifndef crane_h
+#define crane_h
+
+#include "Arduino.h"
+
+void initWiFi();
+void setStaticWifi();
+#endif
--- SemesterProject/ArduinoCode/Shared/wifi.ino
+++ SemesterProject/ArduinoCode/Shared/wifi.ino
@@ -15,3 +15,19 @@ void initWiFi() {
   }
   Serial.println(WiFi.localIP());
 }
+
+// Set your Static IP address
+IPAddress local_IP(192, 168, 1, 184);
+// Set your Gateway IP address
+IPAddress gateway(192, 168, 1, 1);
+
+IPAddress subnet(255, 255, 0, 0);
+IPAddress primaryDNS(8, 8, 8, 8);   // optional
+IPAddress secondaryDNS(8, 8, 4, 4); // optional
+
+void setStaticWifi(){
+    // Configures static IP address
+    if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+    Serial.println("STA Failed to configure");
+    }
+}
