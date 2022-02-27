d3785890cc3bd038ef36b52749b7c3504ea9f5e9
Peter <peter.braendgaard@gmail.com>
2022-02-27 11:04:15 +0100

    Wifi example code

---
+++ SemesterProject/ArduinoCode/Shared/wifi.ino
@@ -0,0 +1,17 @@
+#include "WiFi.h"
+#include "Arduino.h"
+
+// Replace with your network credentials
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
