457835d84a3193832940e71aa6d00e9d66d160d1
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2022-03-11 13:12:26 +0100

    change

--- Linux/Module6/internal_temp/temperature.py
+++ Linux/Module6/internal_temp/temperature.py
@@ -10,5 +10,5 @@ while True:
     led.toggle()
     reading = sensor_temp.read_u16() * conversion_factor
     temperature = 27 - (reading - 0.706)/0.001721
-    print (temperature)
+    print(temperature)
     utime.sleep(2.0)
