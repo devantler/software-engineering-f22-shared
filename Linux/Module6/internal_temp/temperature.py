20270286bbbcc0c5cca466b7fb26fedaf7988309
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2022-03-11 12:57:25 +0100

    added temp

--- Linux/Module6/internal_temp/temperature.py
+++ Linux/Module6/internal_temp/temperature.py
@@ -1,8 +1,11 @@
 import machine
 import utime
+
 led = machine.Pin(16, machine.Pin.OUT)
+
 conversion_factor = 3.3 / (65535)
 sensor_temp = machine.ADC(4)
+
 while True:
     led.toggle()
     reading = sensor_temp.read_u16() * conversion_factor
