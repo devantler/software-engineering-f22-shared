78c4172377fd68592dfd205c2a8813e22b8d1877
Peterzxcvbnm <peter.braendgaard@gmail.com>
2022-03-25 10:38:04 +0100

    Disc example

---
+++ SemesterProject/DotNetApp/Domain/Entities/Slot.cs
@@ -0,0 +1,17 @@
+namespace Domain.Entities;
+
+public class Slot
+{
+    private List<string> _marks = new List<string>();
+    public bool HasItem { get; set; }
+    
+    public bool HasMark(string mark)
+    {
+        return _marks.Contains(mark);
+    }
+    
+    public void AddMark(string mark)
+    {
+        _marks.Add(mark);
+    }
+}
