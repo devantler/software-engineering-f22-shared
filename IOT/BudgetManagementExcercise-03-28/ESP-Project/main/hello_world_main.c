d08eead14461859d13ef5ca18f62d4a3c377e981
Frederik Hounsvad <frederik@hounsvad.dk>
2022-03-28 10:09:17 +0200

    IOT 03-28 Q3

--- IOT/BudgetManagementExcercise-03-28/ESP-Project/main/hello_world_main.c
+++ IOT/BudgetManagementExcercise-03-28/ESP-Project/main/hello_world_main.c
@@ -6,6 +6,9 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
+
+//To get the version of Q2 go to commit "IOT 03-28 Q2"
+
 #include <stdio.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
@@ -14,11 +17,23 @@
 #include "esp_spi_flash.h"
 #include "driver/adc.h"
 #include "freertos/queue.h"
+#include "string.h"
 
 
 #define TX_BUFFER_SIZE 10
+#define AVG_BUFFER_SIZE 10
+#define AVG_WINDOW_SIZE 7
 
 QueueHandle_t tx_queue; //This is a pointer
+QueueHandle_t avg_queue;
+
+
+typedef struct stage_interface_t{
+    QueueHandle_t input;
+    QueueHandle_t output;
+};
+
+struct stage_interface_t avg_pair;
 
 void TaskSample(void* pvParameters){
     QueueHandle_t output_queue = (QueueHandle_t)pvParameters;
@@ -33,6 +48,7 @@ void TaskSample(void* pvParameters){
 
 void TaskTransmit(void* pvParameters){
     QueueHandle_t input_queue = (QueueHandle_t)pvParameters;
+    QueueHandle_t avg_queue = (QueueHandle_t)pvParameters;
     int value;
     while (true)
     {
@@ -42,12 +58,47 @@ void TaskTransmit(void* pvParameters){
     }
 }
 
+void TaskAvg(void* pvParameters){
+    struct stage_interface_t* pair = (struct stage_interface_t*)pvParameters;
+    QueueHandle_t input_queue = pair -> input;
+    QueueHandle_t output_queue = pair -> output;
+    int buffer[AVG_WINDOW_SIZE];
+    memset(buffer, 0, AVG_WINDOW_SIZE*sizeof(buffer[0]));
+    uint8_t index = 0;
+    int sum = 0;
+    //TODO: consume AVG_WINDOW_SIZE-1 message
+    int new;
+    while (true)
+    {
+        for(int i = 0; i < AVG_WINDOW_SIZE-1; i++){ //This is good if sending data is the expensive operation
+            //Recieve
+            while(xQueueReceive(input_queue, &new, 10) != pdPASS);
+            int old = buffer[index];
+            buffer[index] = new;
+            sum += new - old;
+            index = (index + 1) % AVG_WINDOW_SIZE;
+        }
+        //Send
+        int value = sum / AVG_WINDOW_SIZE;
+        while(xQueueSendToBack(output_queue, &value, 10) != pdTRUE);
+        
+    }
+}
+
 void app_main(void)
 {
+    //Initialize the ADC
     adc1_config_width(ADC_WIDTH_BIT_12);
     adc1_config_channel_atten(ADC_CHANNEL_6, ADC_ATTEN_DB_11);
+
+    //Initialize the queues
     tx_queue = xQueueCreate(TX_BUFFER_SIZE, sizeof(int));
-    xTaskCreate(TaskSample, "sampleTask", 4096, tx_queue, 1, NULL);
+    avg_queue = xQueueCreate(AVG_BUFFER_SIZE, sizeof(int));
+    avg_pair = (struct stage_interface_t){avg_queue, tx_queue};
+
+    //Initialize the tasks
+    xTaskCreate(TaskSample, "sampleTask", 4096, avg_queue, 1, NULL);
     xTaskCreate(TaskTransmit, "transmitTask", 4096, tx_queue, 1, NULL);
+    xTaskCreate(TaskAvg, "avgTask", 4096, &avg_pair, 1, NULL);
 }
 
