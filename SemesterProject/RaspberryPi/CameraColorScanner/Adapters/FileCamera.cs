e5bd9ee2a66161eb8729dcfb01e8d14fafd18702
Peterzxcvbnm <peter.braendgaard@gmail.com>
2022-04-01 14:12:42 +0200

    Linux image load

--- SemesterProject/RaspberryPi/CameraColorScanner/Adapters/FileCamera.cs
+++ SemesterProject/RaspberryPi/CameraColorScanner/Adapters/FileCamera.cs
@@ -1,13 +1,23 @@
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.Formats.Png;
+using Image = SixLabors.ImageSharp.Image;
 
 namespace CameraColorScanner.Adapters
 {
     public class FileCamera : ICamera
     {
-        public Task<Bitmap> GetImage()
+        public async Task<Bitmap> GetImage()
         {
-            return Task.FromResult((Bitmap) Image.FromFile("/app/image.bmp"));
+            await using var memStream = new MemoryStream();
+            var img = Image.Load("/app/image.bmp");
+            await img.SaveAsync(memStream, img.GetConfiguration().ImageFormatsManager.FindEncoder(PngFormat.Instance));
+            memStream.Seek(0, SeekOrigin.Begin);
+            return new Bitmap(memStream);
         }
     }
 }
